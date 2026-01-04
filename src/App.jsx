410a5f09eaa8b58f6c147d1e25754cd0e2a0fbae
Dewmini24 <dewminipathirana04@gmail.com>
2026-01-05 01:13:51 +0530

    Update app layout and search page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import SearchPage from "./pages/SearchPage";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Estate Agent App</h1>
-      <SearchPage />
+    <div className="app-layout">
+      
+      <div className="left-panel">
+        <h1 className="app-title">Estate Agent App 🏙️</h1>
+      </div>
+
+      <div className="right-panel">
+        <SearchPage />
+      </div>
+
     </div>
-  );
+
+      );
 }
 
 export default App;
