0ab9cdd680f783506642df73aeecb2f12f7c6bf5
Dewmini24 <dewminipathirana04@gmail.com>
2026-01-05 23:14:37 +0530

    Fix double router issue and App not defined error

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchPage from './pages/SearchPage';
-import PropertyPage from './pages/PropertyPage';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import SearchPage from './pages/SearchPage'
+import PropertyPage from './pages/PropertyPage'
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<SearchPage />} />
-          <Route path="/property/:id" element={<PropertyPage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/property/:id" element={<PropertyPage />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
